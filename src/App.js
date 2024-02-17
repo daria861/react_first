5df8f049f9199a5ebdfe785fe719b425c255f09d
unknown <dashulja861@mail.com>
2024-02-17 10:42:13 +0000

    add img

--- src/App.js
+++
@@ -1,9 +0,0 @@
-// import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  let text = "Lorem Lorem Lorem";
-  return <h1>Hello world!</h1>;
-}
-
-export default App;
---
+++ src/FirstComponent.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./FirstComponent.css";
+
+function FirstComponent() {
+  // let text = "Lorem Lorem Lorem";
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <img src={logo} alt="logo" />
+    </>
+  );
+}
+
+export default FirstComponent;
