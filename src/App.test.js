4cf361470c5623e70650e5153aa7c47bd23289cf
Dariia <50766469+daria861@users.noreply.github.com>
2024-02-20 03:42:51 +0000

    Delete src/App.test.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import FirstComponent from "./FirstComponent";
-
-test("renders learn react link", () => {
-  render(<FirstComponent />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
